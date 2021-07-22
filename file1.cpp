02887cadda13a67a4f12ce89ee81c7b234475174
Michael Goulding <Michael.Goulding@microsoft.com>
2021-07-22 15:19:01 -0700

    initial

---
+++ file1.cpp
@@ -0,0 +1,9 @@
+#include "shared.h"
+
+
+void file1()
+{
+    std::string fullSettingPath("settings.bob");
+
+    c4::csubstr c4SettingPath(fullSettingPath.data(), fullSettingPath.size());
+}
---
+++ shared.h
@@ -0,0 +1,12 @@
+//
+//  Microsoft Confidential Information
+//  Copyright (C) Microsoft Corporation. All Rights Reserved.
+//
+
+#pragma once
+
+#pragma warning(push)
+#pragma warning(disable: 4365) // conversion from 'one' to 'another', signed/unsigned mismatch
+#include <ryml/ryml_std.hpp>
+#include <ryml/ryml.hpp>
+#pragma warning(pop)
